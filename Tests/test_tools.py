54788e891b1222193a47fccaf39cd7c79b16d2d8
chakrireddy1609 <chakrireddy1609@gmail.com>
2021-11-08 18:08:38 +0530

    More Minor Changes

--- Tests/test_tools.py
+++ Tests/test_tools.py
@@ -19,38 +19,46 @@ class Test_Tools(BaseTest):
         wait = WebDriverWait(self.driver, 10)
         wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Client Name']")))
         self.driver.find_element(By.XPATH, "//mat-icon[normalize-space()='build']").click()
+        time.sleep(1)
         self.driver.find_element(By.XPATH, "//span[normalize-space()='Publisher Feeds']").click()
         time.sleep(1)
         count_of_feeds = self.driver.find_elements(By.XPATH, "//span[@class='feedURL']")
         assert len(count_of_feeds) != 0
         self.driver.find_element(By.XPATH, "//mat-icon[normalize-space()='build']").click()
+        time.sleep(1)
         self.driver.find_element(By.XPATH, "//span[normalize-space()='Reports']").click()
         time.sleep(1)
         count_of_td = "//div[@class='m-cell-data ng-star-inserted']"
         assert len(count_of_td) != 0
         self.driver.find_element(By.XPATH, "//mat-icon[normalize-space()='build']").click()
+        time.sleep(1)
         self.driver.find_element(By.XPATH, "//span[normalize-space()='Manage Tags']").click()
         wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//span[text()='Select Client']")))
         self.driver.find_element(By.XPATH, "//mat-icon[normalize-space()='build']").click()
+        time.sleep(1)
         self.driver.find_element(By.XPATH, "//span[normalize-space()='Markups']").click()
         time.sleep(1)
         count_of_td_markup = self.driver.find_elements(By.XPATH, "//div[@class='m-cell-data ng-star-inserted']")
         assert len(count_of_td_markup) != 0
         self.driver.find_element(By.XPATH, "//mat-icon[normalize-space()='build']").click()
+        time.sleep(1)
         self.driver.find_element(By.XPATH, "//span[normalize-space()='Publisher Management']").click()
         wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//h2[text()='Publishers']")))
         time.sleep(3)
         self.driver.find_element(By.XPATH, "//mat-icon[normalize-space()='build']").click()
+        time.sleep(1)
         self.driver.find_element(By.XPATH, "//span[normalize-space()='CD User Management']").click()
         time.sleep(1)
         clients_displayed_cd = self.driver.find_elements(By.XPATH, "//span[@class='mx-l-2 ng-star-inserted']")
         assert len(clients_displayed_cd) != 0
         self.driver.find_element(By.XPATH, "//mat-icon[normalize-space()='build']").click()
+        time.sleep(1)
         self.driver.find_element(By.XPATH, "//span[normalize-space()='Scheduler Tasks']").click()
         time.sleep(1)
         clients_displayed_schedulertasks = self.driver.find_elements(By.XPATH, "//td[@class='ng-star-inserted']")
         assert len(clients_displayed_schedulertasks) != 0
         self.driver.find_element(By.XPATH, "//mat-icon[normalize-space()='build']").click()
+        time.sleep(1)
         self.driver.find_element(By.XPATH, "//span[normalize-space()='User Management']").click()
 
 

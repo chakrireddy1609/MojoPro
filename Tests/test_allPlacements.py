a3df6f4e2297160db8f86b54c821dfb6fe254dcf
chakrireddy1609 <chakrireddy1609@gmail.com>
2021-12-10 16:09:58 +0530

    xpath change in AllPlacements

--- Tests/test_allPlacements.py
+++ Tests/test_allPlacements.py
@@ -1,7 +1,5 @@
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
 
 from Tests.BaseTest import BaseTest
 from Utils import TestData
@@ -13,8 +11,9 @@ class Test_AllPlacements(BaseTest):
         self.driver.find_element(By.XPATH, "//input[@data-placeholder='Password']").send_keys(TestData.password)
         self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
         time.sleep(3)
-        self.driver.find_element(By.XPATH, "//a[@href='/placements']").click()
+        self.driver.find_element(By.XPATH, "//a[text()=' All Publishers ']").click()
         time.sleep(3)
         count_of_placements = self.driver.find_elements(By.XPATH, "//div[@class='m-cell-data ng-star-inserted']/a")
+        time.sleep(3)
         assert len(count_of_placements) != 0
 
